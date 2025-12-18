222c44b38f665170abfaf29af3a3952c7d50fa48
quang <phanvantanquang@gmail.com>
2025-12-18 19:42:53 +0700

    hoan thanh template

--- QKC/myproject/myapp/views.py
+++ QKC/myproject/myapp/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def shop(request):
+    return render(request, 'shop.html')
+
+
+def product_single(request):
+    return render(request, 'product-single.html')
+
+
+def cart(request):
+    return render(request, 'cart.html')
+
+
+def checkout(request):
+    return render(request, 'checkout.html')
+
+
+def wishlist(request):
+    return render(request, 'wishlist.html')
+
+
+def blog(request):
+    return render(request, 'blog.html')
+
+
+def blog_single(request):
+    return render(request, 'blog-single.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
